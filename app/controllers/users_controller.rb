987d14b19bca8001ec0fcc17142e469ed197b6fe
Htoo Aung Hlaing <htooaunghlaing95@gmail.com>
2018-06-15 13:35:52 +0700

    Add singin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to users_path(@user)
     else
       render 'new'
     end
